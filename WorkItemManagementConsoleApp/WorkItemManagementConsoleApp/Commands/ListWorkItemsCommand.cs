cabcc105e93cfda0987019cb24d5e8624ada742c
AngelYankov <angel_yankovv@abv.bg>
2021-02-12 17:22:32 +0200

    listcommand done

--- WorkItemManagementConsoleApp/WorkItemManagementConsoleApp/Commands/ListWorkItemsCommand.cs
+++ WorkItemManagementConsoleApp/WorkItemManagementConsoleApp/Commands/ListWorkItemsCommand.cs
@@ -32,13 +32,21 @@ namespace WorkItemManagementConsoleApp.Commands
                 case "feedback":
                     filteredList = (IList<IWorkItem>)FilterFeedback(this.CommandParameters.Skip(1).ToList());
                     break;
+                case "bug":
+                    filteredList = (IList<IWorkItem>)FilterBug(this.CommandParameters.Skip(1).ToList());
+                    break;
+                case "story":
+                    filteredList = (IList<IWorkItem>)FilterStory(this.CommandParameters.Skip(1).ToList());
+                    break;
+                default:
+                    throw new ArgumentException("Not a valid filtering command");
 
             }
             return string.Join(", ", filteredList);
         }
         private IList<IFeedback> FilterFeedback(IList<string> commands) //new unscheduled scheduled done  //title/rating
         {
-            IList<IFeedback> feedbacks = Validator.GetAllWorkItems().OfType<IFeedback>().ToList();
+            var feedbacks = Validator.GetAllWorkItems().OfType<IFeedback>().ToList();
             if (commands.Count == 0)
             {
                 return feedbacks;
@@ -53,10 +61,10 @@ namespace WorkItemManagementConsoleApp.Commands
                 return SortFeedbackBy(commands[1], feedbacks);
             }
             else throw new ArgumentException("Feedback filter count is not valid.");
-            
+
         }
 
-        private IList<IFeedback> FilterFeedbackByStatus(string status, IList<IFeedback> feedbacks)
+        private List<IFeedback> FilterFeedbackByStatus(string status, List<IFeedback> feedbacks)
         {
             switch (status)
             {
@@ -68,7 +76,7 @@ namespace WorkItemManagementConsoleApp.Commands
                 default: throw new ArgumentException($"Feedback status: '{status}' is not valid");
             }
         }
-        private IList<IFeedback> SortFeedbackBy(string sort, IList<IFeedback> feedbacks)
+        private List<IFeedback> SortFeedbackBy(string sort, List<IFeedback> feedbacks)
         {
             switch (sort)
             {
@@ -80,5 +88,134 @@ namespace WorkItemManagementConsoleApp.Commands
             }
         }
 
+        private List<IBug> FilterBug(IList<string> commands) //active/fixed/assignee
+        {
+            var bugs = Validator.GetAllWorkItems().OfType<IBug>().ToList();
+            if (commands.Count == 0)
+            {
+                return bugs;
+            }
+            else if (commands.Count == 1)
+            {
+                return FilterBugByStatusOrAssignee(commands[0], bugs);
+            }
+            else if (commands.Count == 2)
+            {
+                bugs = FilterBugByStatusOrAssignee(commands[0], bugs);
+                return SortBug(commands[1], bugs);
+            }
+            else if (commands.Count == 3)
+            {
+                bugs = FilterBugByStatusOrAssignee(commands[0], bugs);
+                bugs = SortBug(commands[1], bugs);
+                return SortBug(commands[2], bugs);
+            }
+            else
+            {
+                throw new ArgumentException("Bug filter count is not valid.");
+            }
+        }
+
+        private List<IBug> FilterBugByStatusOrAssignee(string filter, List<IBug> bugs)
+        {
+            switch (filter)
+            {
+                case "active":
+                case "fixed":
+                    return bugs.Where(b => b.Status.ToString().Equals(filter, StringComparison.OrdinalIgnoreCase)).ToList();
+                default:
+                    return FilterBugAssignee(filter, bugs);
+            }
+        }
+
+        private List<IBug> FilterBugAssignee(string filter, List<IBug> bugs)
+        {
+            if (!bugs.Any(b => b.Assignee.Name.Equals(filter, StringComparison.OrdinalIgnoreCase)))
+            {
+                throw new ArgumentException($"There is no filter: '{filter}' for bug.");
+            }
+            return bugs.Where(b => b.Assignee.Name.Equals(filter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private List<IBug> SortBug(string filter, List<IBug> bugs)
+        {
+            switch (filter)
+            {
+                case "title":
+                    return bugs.OrderBy(b => b.Title).ToList();
+                case "severity":
+                    return bugs.OrderBy(b => b.Severity).ToList();
+                case "priority":
+                    return bugs.OrderBy(b => b.Priority).ToList();
+                default:
+                    return FilterBugAssignee(filter, bugs);
+            }
+        }
+
+        private List<IStory> FilterStory(List<string> commands)
+        {
+            var stories = Validator.GetAllWorkItems().OfType<IStory>().ToList();
+            if (commands.Count == 0)
+            {
+                return stories;
+            }
+            else if (commands.Count == 1)
+            {
+                return FilterStoryByStatusOrAssignee(commands[0], stories).ToList();
+            }
+            else if (commands.Count == 2)
+            {
+                stories = FilterStoryByStatusOrAssignee(commands[0], stories);
+                return SortStory(commands[1], stories);
+            }
+            else if (commands.Count == 3)
+            {
+                stories = FilterStoryByStatusOrAssignee(commands[0], stories);
+                stories = SortStory(commands[1], stories);
+                return SortStory(commands[2], stories);
+            }
+            else
+            {
+                throw new ArgumentException("Story filter count is not valid.");
+            }
+        }
+
+        private List<IStory> FilterStoryByStatusOrAssignee(string filter, List<IStory> stories)
+        {
+            switch (filter)
+            {
+                case "notdone":
+                case "inprogress":
+                case "done":
+                    return stories.Where(s => s.StoryStatus.ToString().Equals(filter, StringComparison.OrdinalIgnoreCase)).ToList();
+                default:
+                    return FilterStoryAssignee(filter, stories);
+            }
+        }
+
+        private List<IStory> FilterStoryAssignee(string filter, List<IStory> stories)
+        {
+            if (!stories.Any(s => s.Assignee.Name.Equals(filter, StringComparison.OrdinalIgnoreCase)))
+            {
+                throw new ArgumentException($"There is no filter: '{filter}' for story.");
+            }
+            return stories.Where(s => s.Assignee.Name.Equals(filter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private List<IStory> SortStory(string filter, List<IStory> stories)
+        {
+            switch (filter)
+            {
+                case "title":
+                    return stories.OrderBy(s => s.Title).ToList();
+                case "size":
+                    return stories.OrderBy(s => s.Size).ToList();
+                case "priority":
+                    return stories.OrderBy(s => s.Priority).ToList();
+                default:
+                    return FilterStoryAssignee(filter, stories);
+            }
+        }
     }
 }
+
