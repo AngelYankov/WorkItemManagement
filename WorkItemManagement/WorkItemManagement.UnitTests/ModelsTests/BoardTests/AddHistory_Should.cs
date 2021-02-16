a63cd438da8c1b2f624fe9fd854c0382599055ba
Emil Nenchev <emilnenchev7@gmail.com>
2021-02-16 14:50:07 +0200

    board class partial unit tests

---
+++ WorkItemManagement/WorkItemManagement.UnitTests/ModelsTests/BoardTests/AddHistory_Should.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Models.WorkItems;
+
+namespace WorkItemManagement.UnitTests.ModelsTests.BoardTests
+{
+    [TestClass]
+    public class AddHistory_Should  // AddedSuccessfully_Scenario_ExpectedBehaviour
+    {
+        [TestMethod]
+        public void History_Added()
+        {
+            var board = new Board("Board1");
+            board.ActivityHistory.Add("history");
+            Assert.AreEqual("history", string.Join("",board.ActivityHistory));
+        }
+    }
+}
---
+++ WorkItemManagement/WorkItemManagement.UnitTests/ModelsTests/BoardTests/BoardConstructor_Should.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Models.WorkItems;
+
+namespace WorkItemManagement.UnitTests.ModelsTests.BoardTests
+{
+    [TestClass]
+    public class BoardConstructor_Should
+    {
+        [TestMethod]
+        public void SetProperties()
+        {
+            var board = new Board("Board1");
+            Assert.AreEqual("Board1", board.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowArgumentNullException_NullName()
+        {
+            var board = new Board(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Board name should be between 5 and 10 characters.")]
+        public void ThrowArgumentException_MinLengthName()
+        {
+            var board = new Board(new string('a',4));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Board name should be between 5 and 10 characters.")]
+        public void ThrowArgumentException_MaxLengthName()
+        {
+            var board = new Board(new string('a', 11));
+
+        }
+    }
+}
