0fe18563ef582c3dbe47b7f473bec4d2f039014d
AngelYankov <angel_yankovv@abv.bg>
2021-02-17 22:35:58 +0200

    CreateBug Tests

---
+++ WorkItemManagement/WorkItemManagement.UnitTests/CoreTests/FactoryTests/CreateBug_Should.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Core;
+using WorkItemManagement.Core.Contracts;
+using WorkItemManagement.Models.Contracts;
+using WorkItemManagement.Models.Enums;
+using WorkItemManagement.Models.WorkItems;
+using WorkItemManagement.UnitTests.Cleaner_Should;
+
+namespace WorkItemManagement.UnitTests.CoreTests.FactoryTests
+{
+    [TestClass]
+    public class CreateBug_Should : Cleaner
+    {
+        [TestMethod]
+        public void CreateBug_Should_CreateBugSuccessfully()
+        {
+            var steps = new List<string>() { "first-second-third" };
+            var bug = Factory.Instance.CreateBug("1", "TheFirstBug", "high", "critical", "active", steps, "This is a description for a bug");
+            Assert.IsInstanceOfType(bug, typeof(IBug));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateBug_Should_ThrowWrongPriority()
+        {
+            var steps = new List<string>() { "first-second-third" };
+            Factory.Instance.CreateBug("1", "TheFirstBug", "wrong", "critical", "active", steps, "This is a description for a bug");
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateBug_Should_ThrowWrongSeverity()
+        {
+            var steps = new List<string>() { "first-second-third" };
+            Factory.Instance.CreateBug("1", "TheFirstBug", "high", "wrong", "active", steps, "This is a description for a bug");
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateBug_Should_ThrowWrongStatus()
+        {
+            var steps = new List<string>() { "first-second-third" };
+            Factory.Instance.CreateBug("1", "TheFirstBug", "high", "critical", "wrong", steps, "This is a description for a bug");
+        }
+
+    }
+}
