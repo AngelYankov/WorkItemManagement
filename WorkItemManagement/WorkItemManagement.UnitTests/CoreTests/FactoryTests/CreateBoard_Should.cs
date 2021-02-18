21757a1d62009a3cab6ea847ba33834d74f7ef2b
AngelYankov <angel_yankovv@abv.bg>
2021-02-18 11:36:42 +0200

    CreateBoard and CreateFeedback tests created

---
+++ WorkItemManagement/WorkItemManagement.UnitTests/CoreTests/FactoryTests/CreateBoard_Should.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Core;
+using WorkItemManagement.Models.Contracts;
+
+namespace WorkItemManagement.UnitTests.CoreTests.FactoryTests
+{
+    [TestClass]
+    public class CreateBoard_Should
+    {
+        [TestMethod]
+        public void CreateBoard_Should_CreateBoardSuccessfully()
+        {
+            var board = Factory.Instance.CreateBoard("board1");
+            Assert.IsInstanceOfType(board, typeof(IBoard));
+        }
+    }
+}
---
+++ WorkItemManagement/WorkItemManagement.UnitTests/CoreTests/FactoryTests/CreateFeedback_Should.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkItemManagement.Core;
+using WorkItemManagement.Models.Contracts;
+using WorkItemManagement.UnitTests.Cleaner_Should;
+
+namespace WorkItemManagement.UnitTests.CoreTests.FactoryTests
+{
+    [TestClass]
+    class CreateFeedback_Should : Cleaner
+    {
+        [TestMethod]
+        public void CreateFeedback_Should_CreateFeedbackSuccessfully()
+        {
+            var feedback = Factory.Instance.CreateFeedback("1", "Feedback1", 3, "This is a description for feedback");
+            Assert.IsInstanceOfType(feedback, typeof(IFeedback));
+        }
+    }
+}
