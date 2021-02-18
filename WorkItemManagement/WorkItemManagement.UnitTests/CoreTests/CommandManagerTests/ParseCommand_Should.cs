97b9ee0ed4442d378b81875e7cc571fca3a6cb83
AngelYankov <angel_yankovv@abv.bg>
2021-02-18 17:21:13 +0200

    CommandManager Tests done

---
+++ WorkItemManagement/WorkItemManagement.UnitTests/CoreTests/CommandManagerTests/ParseCommand_Should.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Commands.Contracts;
+
+namespace WorkItemManagement.UnitTests.CoreTests.CommandManagerTests
+{
+    [TestClass]
+    public class ParseCommand_Should
+    {
+        [TestMethod]
+        [DataRow("createteam")]
+        [DataRow("createmember")]
+        [DataRow("createboard")]
+        [DataRow("createbug")]
+        [DataRow("createstory")]
+        [DataRow("createfeedback")]
+        [DataRow("showallmembers")]
+        [DataRow("showallteams")]
+        [DataRow("showallboards")]
+        [DataRow("showpersonactivity")]
+        [DataRow("showallteammembers")]
+        [DataRow("showteamactivity")]
+        [DataRow("showboardactivity")]
+        [DataRow("listworkitems")]
+        [DataRow("addmember")]
+        [DataRow("addcomment")]
+        [DataRow("assign")]
+        [DataRow("unassign")]
+        [DataRow("changebug")]
+        [DataRow("changestory")]
+        [DataRow("changefeedback")]
+        [DataRow("help")]
+        public void ParseCommand_Should_ReturnICommand(string input)
+        {
+            var commandManager = new Core.CommandManager();
+            var command = commandManager.ParseCommand(input);
+            Assert.IsInstanceOfType(command, typeof(ICommand));
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ParseCommand_Should_ThrowInvalidOperationException_WrongCommand()
+        {
+            var commandManager = new Core.CommandManager();
+            commandManager.ParseCommand("wrongCommand");
+        }
+    }
+}
