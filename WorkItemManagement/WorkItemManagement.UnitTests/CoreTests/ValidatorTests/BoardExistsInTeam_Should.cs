eaca21df4e29f2a21f47e6391deb5b6fe693a055
Emil Nenchev <emilnenchev7@gmail.com>
2021-02-19 12:55:07 +0200

    part of validator tests

---
+++ WorkItemManagement/WorkItemManagement.UnitTests/CoreTests/ValidatorTests/BoardExistsInTeam_Should.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Core;
+using WorkItemManagement.Models.WorkItems;
+
+namespace WorkItemManagement.UnitTests.CoreTests.ValidatorTests
+{
+    [TestClass]
+    public class BoardExistsInTeam_Should
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ThrowWhen_BoardExists()
+        {
+            //public static void BoardExistsInTeam(string name,ITeam team) // emo
+            var team = new Team("Team1");
+            team.AddBoard(new Board("Board1"));
+            Validator.BoardExistsInTeam("Board1", team);
+        }
+    }
+}
---
+++ WorkItemManagement/WorkItemManagement.UnitTests/CoreTests/ValidatorTests/GetWorkItemToAssign_Should.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkItemManagement.Core;
+using WorkItemManagement.Models.Contracts;
+using WorkItemManagement.Models.Enums;
+using WorkItemManagement.Models.WorkItems;
+
+namespace WorkItemManagement.UnitTests.CoreTests.ValidatorTests
+{
+    [TestClass]
+    public class GetWorkItemToAssign_Should
+    {
+        [TestMethod]
+        //public static IWorkItemsAssignee GetWorkItemToAssign(string id, IDatabase database)
+        public void GetWorkItemToAssign_ReturnsWorkItem()
+        {
+            var fakeData = new FakeDatabase();
+            var workItem = new Bug("1", "Bug 1 fixing", PriorityType.High, SeverityType.Critical, new List<string>(), "Bug 1 description.");
+            fakeData.AddWorkItem(workItem);
+            var workItemtoAssign = Validator.GetWorkItemToAssign("1", fakeData);
+            Assert.IsInstanceOfType(workItemtoAssign, typeof(IWorkItemsAssignee));
+            Assert.AreEqual(workItem, workItemtoAssign);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetWorkItemToAssign_ThrowWhen_WorkItemIsFeedback()
+        {
+            var fakeData = new FakeDatabase();
+            var feedback = new Feedback("1", "Feedback Number 1", 5, "Feedback 1 description.");
+            fakeData.AddWorkItem(feedback);
+            Validator.GetWorkItemToAssign("1", fakeData);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetWorkItemToAssign_ThrowWhen_WorkItemNotExist()
+        {
+            var fakeData = new FakeDatabase();
+            var workItem = new Bug("1", "Bug 1 fixing", PriorityType.High, SeverityType.Critical, new List<string>(), "Bug 1 description.");
+            fakeData.AddWorkItem(workItem);
+            Validator.GetWorkItemToAssign("2", fakeData);
+        }
+
+    }
+}
